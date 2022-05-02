ec67a5e852b25356f571d5810f835dd2e61be249
kang myung jin <kangel429@nate.com>
2022-05-02 21:19:26 +0900

    광고 적용

--- js/share.js
+++ js/share.js
@@ -3,8 +3,17 @@ const title = '[반려견의 mbti] 반려견의 타고난 기질은?';
 const hash = '%23반려견 %23반려견성격 %23반려견테스트 ';
 
 const fb = () => {
-  window.open('https://www.facebook.com/sharer/sharer.php?u='
-    + url + '&t=' + title + '' + hash,
+    let name = document.querySelector('.result').innerHTML;
+  switch (name) {
+    case '표범':
+    case '유니콘':
+      name += '이에요! ';
+      break;
+    default:
+      name += '예요! ';
+  }
+  window.open('https://www.facebook.com/sharer/sharer.php?u='+ '저의 반려견은 '
+    + title + '%0A' + '저의 반려견은 ' + name + '' + hash + '%0A' + url,
     'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
   return false;
 }
@@ -24,9 +33,30 @@ const tw = () => {
   return false;
 }
 const nv = () => {
-  window.open('http://share.naver.com/web/shareView.nhn?url='
-    + url + '&title=' + title,
-    'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+  // window.open('http://share.naver.com/web/shareView.nhn?url='
+  //   + url + '&title=' + title,
+  //   'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+    let name = document.querySelector('.result').innerHTML;
+  switch (name) {
+    case '표범':
+    case '유니콘':
+      name += '이에요! ';
+      break;
+    default:
+      name += '예요! ';
+  }
+  Kakao.Link.sendDefault({
+  objectType: "feed",
+  content: {
+    title: title, // 공유될 제목
+    description:  "저의 반려견은 "+name, // 공유될 설명
+    imageUrl: "", // 공유될 이미지 url
+    link: {
+      mobileWebUrl: url, // 공유될 모바일 URL
+      webUrl: url, // 공유될 웹 URL
+    },
+  },
+});
   return false;
 }
 const band = () => {
