6149e0bdc187b8f2f4d07c82201bd5ad766e6ce2
kang myung jin <kangel429@nate.com>
2022-05-02 21:47:24 +0900

    광고 적용

--- js/share.js
+++ js/share.js
@@ -12,8 +12,8 @@ const fb = () => {
     default:
       name += '예요! ';
   }
-  window.open('https://www.facebook.com/sharer/sharer.php?u='+ '저의 반려견은 '
-    + title + '%0A' + '저의 반려견은 ' + name + '' + hash + '%0A' + url,
+  window.open('https://www.facebook.com/sharer/sharer.php?u='
+    + url + '&t=' + title + '%0A' + '저의 반려견은 ' + name + '' + hash,
     'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
   return false;
 }
@@ -33,31 +33,9 @@ const tw = () => {
   return false;
 }
 const nv = () => {
-  // window.open('http://share.naver.com/web/shareView.nhn?url='
-  //   + url + '&title=' + title,
-  //   'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
-    let name = document.querySelector('.result').innerHTML;
-  switch (name) {
-    case '표범':
-    case '유니콘':
-      name += '이에요! ';
-      break;
-    default:
-      name += '예요! ';
-  }
-  window.Kakao.init('4bade7459a7f9b3f550f30f712bab58c');
-  window.Kakao.Link.createDefaultButton({
-  objectType: "feed",
-  content: {
-    title: title, // 공유될 제목
-    description:  "저의 반려견은 "+name, // 공유될 설명
-    imageUrl: "", // 공유될 이미지 url
-    link: {
-      mobileWebUrl: url, // 공유될 모바일 URL
-      webUrl: url, // 공유될 웹 URL
-    },
-  },
-});
+  window.open('http://share.naver.com/web/shareView.nhn?url='
+    + url + '&title=' + title,
+    'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
   return false;
 }
 const band = () => {
