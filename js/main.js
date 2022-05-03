550da6e0909987ec6c3b715c4195d0ef8f71ae7e
kang myung jin <kangel429@nate.com>
2022-05-04 02:28:14 +0900

    로그

--- js/main.js
+++ js/main.js
@@ -9,11 +9,9 @@ const ENDPOINT = 80;
 const select = [];
 let qIdx = -1;
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore/lite";
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
