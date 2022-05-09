425abb736d464a2a091ba19b2f939d009b93c415
kang myung jin <kangel429@nate.com>
2022-05-09 15:41:56 +0900

    s

--- js/main.js
+++ js/main.js
@@ -297,7 +297,6 @@ const goNext = () => {
     end();
     return;
   }
-console.log("다음단계11");
   const status = document.querySelector('.status');
   const qNum = qnaList[qIdx];
   const q = document.querySelector('.q');
